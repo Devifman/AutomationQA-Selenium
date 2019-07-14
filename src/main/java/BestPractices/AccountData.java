93cc412c75f7366b0edf3da00e329db180300fe2
Devifman <Devifman@gmail.com>
2019-07-15 02:20:08 +0400

    Finish AccountData class

--- src/main/java/BestPractices/AccountData.java
+++ src/main/java/BestPractices/AccountData.java
@@ -1,5 +1,80 @@
 package BestPractices;
 
+import org.testng.annotations.DataProvider;
+
 public class AccountData {
+    private String gender;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String date;
+    private String month;
+    private String year;
+    private String company;
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String info;
+    private String homePhone;
+    private String mobilePhone;
+    private String alias;
+
+    @DataProvider(name = "RegData")
+    public Object[][] dataProviderToUser() {
+        return new Object[][]{
+                {new AccountData()}
+        };
+    }
+
+    public String getFirstName(){
+        return "Vasya";
+    }
+    public String getLastName(){
+        return "Pupkin";
+    }
+    public String getPassword(){
+        return "55555";
+    }
+
+    public String getAddress1(){
+        return "55 Avenue";
+    }
+    public String getAddress2(){
+        return "house 5 app 4";
+    }
+
+    public String getCity(){
+        return "Boston";
+    }
+    public String getState(){
+        return "1";
+    }
+
+    public String getZipCode(){
+        return "11111";
+    }
+
+    public String getMobilePhone(){
+        return "89012348645";
+    }
+    public String getHomePhone(){
+        return "222444";
+    }
+    public String getAlias(){
+        return "This Is my Address";
+    }
+
+    public AccountData(){
+        this.firstName = getFirstName();
+        this.lastName = getLastName();
+        this.password = getPassword();
+        this.address1 = getAddress1();
+        this.address2 = getAddress2();
+        this.city = getCity();
+        this.state = getState();
+    }
+
 
 }
--- src/test/java/BestPractices/CreateAnAccountTest.java
+++ src/test/java/BestPractices/CreateAnAccountTest.java
@@ -5,28 +5,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
 
 public class CreateAnAccountTest {
 private WebDriver driver;
 private WebDriverWait waiter;
-private By emailLocator = By.xpath("//*[@id=\"email_create\"]");
-private By createAnAccountButton = By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/div[1]/form/div/div[3]/button/span");
+private CreateAnAccountPage createAnAccountPage;
 
-public CreateAnAccountTest(WebDriver driver){
-    this.driver = driver;
-    this.waiter = new WebDriverWait(driver, 30);
-
-}
-public void SignUpWith(String email){
-    WebElement emailField = findElement(emailLocator);
-    emailField.click();
-    emailField.sendKeys(email);
-    WebElement signUp = findElement(createAnAccountButton);
-    signUp.click();
-}
-private WebElement findElement(By locator){
-    return waiter.until(ExpectedConditions.visibilityOfElementLocated(locator));
-
-}
+//@BeforeClass
+    //public void beforeClassMethod
 
 }
