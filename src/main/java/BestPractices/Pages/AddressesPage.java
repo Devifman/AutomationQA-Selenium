b5ca5da9c6e0ce7aecbc7a7fd1c76e722efb1f18
Devifman <Devifman@gmail.com>
2019-07-19 02:21:02 +0400

    addMethods

--- src/main/java/BestPractices/Pages/AddressesPage.java
+++ src/main/java/BestPractices/Pages/AddressesPage.java
@@ -1,5 +1,6 @@
 package BestPractices.Pages;
 
+import BestPractices.Models.AccountData;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -17,9 +18,9 @@ public class AddressesPage extends RegPage {
     @FindBy(xpath = "/html/body/div[1]/div[2]/div/div[3]/div/div[1]/div/div/ul/li[3]/span")
     private WebElement companyAddressField;
     @FindBy(xpath = "/html/body/div[1]/div[2]/div/div[3]/div/div[1]/div/div/ul/li[4]/span[1]")
-    private WebElement addressLine1;
+    private WebElement addressLine1Field;
     @FindBy(xpath = "/html/body/div[1]/div[2]/div/div[3]/div/div[1]/div/div/ul/li[4]/span[2]")
-    private WebElement addressLine2;
+    private WebElement addressLine2Field;
     @FindBy (xpath = "/html/body/div[1]/div[2]/div/div[3]/div/div[1]/div/div/ul/li[5]/span[1]")
     private WebElement cityAddressField;
     @FindBy (xpath = "/html/body/div[1]/div[2]/div/div[3]/div/div[1]/div/div/ul/li[5]/span[1]" +
@@ -33,4 +34,45 @@ public class AddressesPage extends RegPage {
     private WebElement mobilePhoneField;
     @FindBy(xpath = "//p[@class='submit2']")
     private WebElement sumbit;
+
+    public boolean firstNameEquals(AccountData accountData){
+        return firstNameAddressField.getText().contentEquals(accountData.getFirstName());
+    }
+
+    public boolean lastNameEquals(AccountData accountData){
+        return lastNameAddressField.getText().contentEquals(accountData.getLastName());
+    }
+
+    public boolean companyEquals(AccountData accountData){
+        return companyAddressField.getText().contentEquals(accountData.getCompany());
+    }
+
+    public boolean addressLine1Equals(AccountData accountData){
+        return addressLine1Field.getText().contentEquals(accountData.getAddress1());
+    }
+
+    public boolean addressLine2Equals(AccountData accountData){
+        return addressLine2Field.getText().contentEquals(accountData.getAddress2());
+    }
+
+    public boolean cityEquals(AccountData accountData){
+        return cityAddressField.getText().contentEquals(accountData.getCity());
+    }
+
+    public boolean zipCodeEquals(AccountData accountData){
+        return zipCodeField.getText().contentEquals(accountData.getZipCode());
+    }
+
+    public boolean homePhoneEquals(AccountData accountData){
+        return homePhoneField.getText().contentEquals(accountData.getHomePhone());
+    }
+
+    public boolean mobilePhoneEquals(AccountData accountData){
+        return mobilePhoneField.getText().contentEquals(accountData.getMobilePhone());
+    }
+
+
+    public void clickSumbit(){
+        sumbit.click();
+    }
 }
